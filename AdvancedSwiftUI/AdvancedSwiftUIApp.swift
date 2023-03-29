458a0b85c50aa677198ed535653ac4f6e87ec662
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-03-29 07:04:50 -0500

    Initial Commit

---
+++ AdvancedSwiftUI/AdvancedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIApp.swift
+//  AdvancedSwiftUI
+//
+//  Created by Justin Hold on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
